ca8dcc0fa288b29a0623e47c1901146b16b2595a
Ibrahim <ibrahimmiishaq@gmail.com>
2025-02-04 19:55:06 -0800

    Camera & Matrix Functionality [P I]

--- Pix/Pix/CommandDictionary.cpp
+++ Pix/Pix/CommandDictionary.cpp
@@ -14,6 +14,8 @@
 #include "CmdShowViewport.h"
 #include "CmdSetClipping.h"
 
+#include "CmdMatrix.h"
+
 CommandDictionary* CommandDictionary::Get()
 {
 	static CommandDictionary sInstance;
@@ -24,26 +26,34 @@ CommandDictionary::CommandDictionary()
 {
 	// Initialize dictionary
 
-	// Setting commands
+	// Setting Commands
 	RegisterCommand<CmdSetResolution>();
 
-	// Variable commands
+	// Variable Commands
 	RegisterCommand<CmdVarFloat>();
 
-	// Rasterization commands
+	// Rasterization Commands
 	RegisterCommand<CmdDrawPixel>();
 	RegisterCommand<CmdSetColor>();
 	RegisterCommand<CmdSetFillMode>();
 
-	// Primatives commands
+	// Primatives Commands
 	RegisterCommand<CmdBeginDraw>();
 	RegisterCommand<CmdEndDraw>();
 	RegisterCommand<CmdAddVertex>();
 
-	// Viewport commands
+	// Viewport Commands
 	RegisterCommand<CmdSetViewport>();
 	RegisterCommand<CmdShowViewport>();
 	RegisterCommand<CmdSetClipping>();
+
+	// Matrix Commands
+	RegisterCommand<CmdPushTranslation>();
+	RegisterCommand<CmdPushRotationX>();
+	RegisterCommand<CmdPushRotationY>();
+	RegisterCommand<CmdPushRotationZ>();
+	RegisterCommand<CmdPushScaling>();
+	RegisterCommand<CmdPopMatrix>();
 
 }
 
